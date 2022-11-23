4bf9457d083765ca3567f5e1b4d979da2801c5ab
Levou7 <107188165+Levou7@users.noreply.github.com>
2022-11-23 20:52:39 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+// let href = document.querySelector('.insta')
+
+// href.addEventListener('click', e=>{
+//     e.preventDefault()
+//     console.log(e.target.id);
+//     alert('Разрешить')
+//     href.setAttribute("download")
+//     console.log(href.getAttribute("download"));
+// })
